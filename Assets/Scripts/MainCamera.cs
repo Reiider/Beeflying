59db827c2c0680912de3da36747bf550a0ad7a82
Vasily <Hapankoff@yandex.ru>
2020-06-15 22:24:03 +0300

    add moving camera and spawn fances

---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using Random = System.Random;
+
+public class MainCamera : MonoBehaviour
+{
+    public float startSpeed = 1;
+    float speed;
+    float currentY = 0;
+    public float acceleration = 1; //accelerating every 10 points
+    public GameObject bottom;
+    public GameObject player;
+    public GameObject[] grounds;
+
+    public GameObject fence;
+    private List<GameObject>[] fences;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = startSpeed;
+        player.GetComponent<Player>().IncreaseSpeed(speed * Time.deltaTime);
+        
+        fences = new List<GameObject>[grounds.Length];
+        for(int i = 0; i < grounds.Length; i++)
+        {
+            fences[i] = new List<GameObject>();
+        }
+        ReCreateFence(1, 10);
+        ReCreateFence(2, 20);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.transform.position.y < bottom.transform.position.y) Death();
+
+        player.GetComponent<Player>().IncreaseSpeed(speed * Time.deltaTime);
+        Vector3 position = transform.position;
+        if(position.y > currentY + 10)
+        {
+            ResetGrounds(currentY);
+            currentY += 10;
+            speed += acceleration;
+            player.GetComponent<Player>().IncreaseSpeed(speed * Time.deltaTime);
+        }
+        position.y += speed * Time.deltaTime;
+        transform.position = Vector3.Lerp(transform.position, position, speed * Time.deltaTime);
+    }
+
+    private void ResetGrounds(float y)
+    {
+        int i = (int) ((y % 30) / 10);
+        print(i);
+        grounds[i].transform.position = new Vector3(grounds[i].transform.position.x,
+                                                    grounds[i].transform.position.y + 30,
+                                                    grounds[i].transform.position.z);
+        ReCreateFence(i, y + 30);
+    }
+
+    private void Death()
+    {
+        print("СМЕЕЕРть");
+    }
+
+    private void ReCreateFence(int index, float y)
+    {
+        for(int i = 0; i < fences[index].Count; i++)
+        {
+            Destroy(fences[index][i]);
+        }
+        fences[index].Clear();
+
+        Random random = new Random();
+
+        for(int i = random.Next(3,9); i >= 0; i--)
+        {
+            GameObject obj = Instantiate(fence);
+            obj.transform.position = new Vector3(random.Next(-65, 65) / 10.0f,
+                                             y + random.Next(-45, 45) / 10.0f,
+                                             obj.transform.position.z);
+            fences[index].Add(obj);
+        }
+    } 
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,21 +5,30 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed;
+    float flySpeed;
     Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        flySpeed = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 position = transform.position;
+        position.y += flySpeed;
+        transform.position = Vector3.Lerp(transform.position, position, flySpeed);
     }
 
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed);
+    }
+
+    public void IncreaseSpeed(float inc)
+    {
+        flySpeed = inc; 
     }
 }
